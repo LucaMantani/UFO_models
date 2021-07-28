5e2234c62758be163bf87709e4fb814cf830a653
Luca Mantani <luca.mantani@gmail.com>
2021-07-28 12:11:17 +0200

    DMsimpt with electrons

---
+++ DMsimp_t_lep/DMsimpt_xm_e_UFO/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.36
+# Mathematica version: 12.1.1 for Mac OS X x86 (64-bit) (June 19, 2020)
+# Date: Wed 28 Jul 2021 11:47:09
+
+
+from object_library import all_orders, CouplingOrder
+
+
+DMT = CouplingOrder(name = 'DMT',
+                    expansion_order = 2,
+                    hierarchy = 2)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
